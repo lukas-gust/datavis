717d74fd65496519af2f35838e6bbc03c625c12d
Lukas Gust <lukasgust31@gmail.com>
2019-09-12 06:58:56 -0600

    finished all four parts of the graph

--- Homework 3/script.js
+++ Homework 3/script.js
@@ -49,16 +49,22 @@ function update(data) {
     .scaleLinear()
     .domain([0, d3.max(data, d => d.a)])
     .range([0, 300]);
+
   let bScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.b)])
     .range([0, 300]);
+
+  let bScaleReverse = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, d => d.b)])
+    .range([300, 0]);
+
   let iScale = d3
     .scaleLinear()
     .domain([0, 11])
     .range([0, 220]);
 
-  
 
   // ****** TODO: PART III (you will also edit in PART V) ******
 
@@ -86,7 +92,7 @@ function update(data) {
 
   aBarRects
     .attr("transform", (d, i) => `translate(18, ${iScale(i)}) scale(-1, 1)`)
-    .transition().duration(200)
+    .transition().duration(500)
     .attr("width", (d, i) => aScale(d.a))
     .attr("height", 18);
 
@@ -115,9 +121,11 @@ function update(data) {
 
   bBarRects
     .attr("transform", (d, i) => `translate(0, ${iScale(i)})`)
-    .transition().duration(200)
+    .transition().duration(500)
     .attr("width", (d, i) => bScale(d.b))
     .attr("height", 18);
+
+
 
   // TODO: Select and update the 'a' line chart path using this line generator
 
@@ -129,7 +137,17 @@ function update(data) {
   let aLineChart = d3.select("#aLineChart");
 
   aLineChart
-    .attr("d", aLineGenerator(data));
+    .attr("d", aLineGenerator(data))
+
+  let aLineLength = aLineChart.node().getTotalLength();
+
+  aLineChart
+    .style("stroke-dasharray", `${aLineLength} ${aLineLength}`)
+    .style("stroke-dashoffset", aLineLength)
+    .transition()
+    .duration(1500)
+    .style("stroke-dashoffset", 0);
+
 
   // TODO: Select and update the 'b' line chart path (create your own generator)
   let bLineGenerator = d3
@@ -142,6 +160,17 @@ function update(data) {
   bLineChart
     .attr("d", bLineGenerator(data));
 
+  let bLineLength = bLineChart.node().getTotalLength();
+
+  bLineChart
+    .style("stroke-dasharray", `${bLineLength} ${bLineLength}`)
+    .style("stroke-dashoffset", bLineLength)
+    .transition()
+    .duration(1500)
+    .style("stroke-dashoffset", 0);
+
+
+
   // TODO: Select and update the 'a' area chart path using this area generator
   let aAreaGenerator = d3
     .area()
@@ -149,12 +178,153 @@ function update(data) {
     .y0(0)
     .y1(d => aScale(d.a));
 
+  let aAreaChart = d3.select("#aAreaChart");
+
+  if (aAreaChart.attr("d") === null) {
+    aAreaChart
+      .style("fill-opacity", 0)
+      .style("stroke", "black")
+      .style("stroke-width", 1)
+      .attr("d", aAreaGenerator(data));
+
+    let aPerimeter = aAreaChart.node().getTotalLength();
+
+    aAreaChart
+      .style("stroke-dasharray", `${aPerimeter} ${aPerimeter}`)
+      .style("stroke-dashoffset", aPerimeter)
+      .transition()
+      .duration(1000)
+      .style("stroke-dashoffset", 0)
+      .transition()
+      .duration(500)
+      .style("fill-opacity", 1)
+      .style("stroke", "#979696")
+      .style("stroke-width", 0.05);
+
+  }
+  else {
+    aAreaChart
+      .transition()
+      .duration(1500)
+      .attrTween('d', function () {
+        var previous = aAreaChart.attr('d');
+        var current = aAreaGenerator(data);
+        return d3.interpolatePath(previous, current)
+      });
+  }
+
+
   // TODO: Select and update the 'b' area chart path (create your own generator)
+  let bAreaGenerator = d3
+    .area()
+    .x((d, i) => iScale(i))
+    .y0(0)
+    .y1(d => bScale(d.b));
+
+  let bAreaChart = d3.select("#bAreaChart");
+
+  if (bAreaChart.attr("d") === null) {
+    bAreaChart
+      .style("fill-opacity", 0)
+      .style("stroke", "black")
+      .style("stroke-width", 1)
+      .attr("d", bAreaGenerator(data));
+
+    let bPerimeter = bAreaChart.node().getTotalLength();
+
+    bAreaChart
+      .style("stroke-dasharray", `${bPerimeter} ${bPerimeter}`)
+      .style("stroke-dashoffset", bPerimeter)
+      .transition()
+      .duration(1000)
+      .style("stroke-dashoffset", 0)
+      .transition()
+      .duration(500)
+      .style("fill-opacity", 1)
+      .style("stroke", "#979696")
+      .style("stroke-width", 0.05);
+
+  }
+  else {
+    bAreaChart
+      .transition()
+      .duration(1500)
+      .attrTween('d', function () {
+        var previous = bAreaChart.attr('d');
+        var current = bAreaGenerator(data);
+        return d3.interpolatePath(previous, current)
+      });
+  }
+
+
 
   // TODO: Select and update the scatterplot points
+  let regressionPoints = [
+    {x:  2, y: 4 },
+    {x: 16, y: 11}
+  ];
 
-  // ****** TODO: PART IV ******
-}
+  let regressionLine = d3
+    .line()
+    .x(d => aScale(d.x))
+    .y(d => -bScale(d.y));
+
+  let scatterplot = d3.select("#scatterplot");
+
+  let regression = scatterplot.select("#regression-line");
+
+  regression
+    .transition()
+    .duration(1000)
+    .attr("d", regressionLine(regressionPoints));
+
+  let aAxis = d3.axisBottom().scale(aScale);
+
+  scatterplot.select("#x-axis").transition().duration(1000).call(aAxis);
+
+  let bAxis = d3.axisLeft().scale(bScaleReverse);
+
+  scatterplot.select("#y-axis").transition().duration(1000).call(bAxis);
+
+  let points = scatterplot.selectAll("circle").data(data);
+
+  let newPoints = points
+    .enter()
+    .append("circle")
+    .style("opacity", 0)
+    .attr("r", "2%");
+
+  newPoints
+    .attr("cx", d => aScale(d.a))
+    .attr("cy", d => -bScale(d.b))
+    .transition()
+    .duration(1000)
+    .style("opacity", 1);
+
+  points
+    .exit()
+    .style("opacity", 1)
+    .transition()
+    .duration(1000)
+    .style("opacity", 0)
+    .remove();
+
+  points = newPoints.merge(points);
+
+  points
+    .transition()
+    .duration(1000)
+    .attr("cx", d => aScale(d.a))
+    .attr("cy", d => -bScale(d.b))
+    .style("opacity", 1);
+
+  points
+    .join()
+    .append("title")
+    .text(d => `(${d.a}, ${d.b})`);
+
+  points.on("click", d => console.log(`(${d.a}, ${d.b})`));
+
 
 /**
  * Update the data according to document settings
